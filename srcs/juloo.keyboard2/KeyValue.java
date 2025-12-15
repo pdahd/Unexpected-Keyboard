ab8cd524a45bb547ef0c79565d005e4a8cdc273f
Terry <168416857+pdahd@users.noreply.github.com>
2025-12-15 16:07:59 +0800

    Add case for switch_numeric_123 event key

--- srcs/juloo.keyboard2/KeyValue.java
+++ srcs/juloo.keyboard2/KeyValue.java
@@ -609,6 +609,7 @@ public final class KeyValue implements Comparable<KeyValue>
       case "config": return eventKey(0xE004, Event.CONFIG, FLAG_SMALLER_FONT);
       case "switch_text": return eventKey("ABC", Event.SWITCH_TEXT, FLAG_SMALLER_FONT);
       case "switch_numeric": return eventKey("123+", Event.SWITCH_NUMERIC, FLAG_SMALLER_FONT);
+      case "switch_numeric_123": return eventKey("123", Event.SWITCH_NUMERIC, FLAG_SMALLER_FONT);
       case "switch_emoji": return eventKey(0xE001, Event.SWITCH_EMOJI, FLAG_SMALLER_FONT);
       case "switch_back_emoji": return eventKey("ABC", Event.SWITCH_BACK_EMOJI, 0);
       case "switch_clipboard": return eventKey(0xE017, Event.SWITCH_CLIPBOARD, 0);
